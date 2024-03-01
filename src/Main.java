f5141fe9a5d5e2d04eb4411b0f160559a30f0c4c
Ingrid <ingridsoaress765@gmail.com>
2024-03-01 15:14:19 -0300

    Atividade_Peso_ideal

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,82 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+/*
+Algoritmo que mostra o o peso ideal, utilizando as seguintes fórmulas:
+
+Sexo feminino: peso ideal = (72.7 * altura) - 58
+Sexo masculino: peso ideal =  (62.1 * altura) - 44.7
+
+*/
+
+public class Main {
+
+    public static double pesoMasc(double altura){
+        return (72.7 * altura) - 58;
+    }
+
+    public static double pesoFem (double altura){
+        return (62.1 * altura) - 44.7;
+    }
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("Programa para saber seu peso ideal!");
+
+        boolean generoValido = false;
+        String genero = " ";
+
+        while(!generoValido) {
+            System.out.println("Digite seu gênero: ");
+            genero = scanner.nextLine();
+
+
+            if (!genero.equalsIgnoreCase("M") && !genero.equalsIgnoreCase("F")) {
+                System.out.println("Gênero errado");
+            }
+            else {
+                generoValido = true;
+            }
         }
+
+        double altura = 0;
+        boolean alturaValida = false;
+
+        while (!alturaValida) {
+            System.out.println("Digite sua altura: ");
+            String alturaConsole = scanner.nextLine();
+
+            try {
+                altura = Double.parseDouble(alturaConsole);
+                alturaValida = true;
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Altura errada!");
+            }
+        }
+
+        double peso = 0;
+
+        switch (genero.toUpperCase()) {
+            case "M":
+                peso = pesoMasc(altura);
+                break;
+
+            case "F":
+                peso = pesoFem(altura);
+                break;
+
+            default:
+                System.out.println("Burrão");
+                break;
+        }
+
+        System.out.println("Seu peso ideal é: " + String.format("%.2f", peso));
+        scanner.close();
+
     }
 }
+
+
+
+
